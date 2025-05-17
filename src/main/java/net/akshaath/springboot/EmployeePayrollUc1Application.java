a43c8949d5995e59639a077d2231a89d6621c85b
Akshaath <nakshaath@gmail.com>
2025-05-17 14:17:30 +0530

    Employee-Payroll-App

---
+++ src/main/java/net/akshaath/springboot/EmployeePayrollUc1Application.java
@@ -0,0 +1,13 @@
+package net.akshaath.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollUc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollUc1Application.class, args);
+	}
+
+}
